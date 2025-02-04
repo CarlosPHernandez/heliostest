7c111330e81b6a0879d21d1ecc7a501d8fdfe623
carlos <carlos@heliosnexus.com>
2025-02-04 04:29:30 -0500

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['maps.googleapis.com'],
+    unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  basePath: '/heliosai'
 }
 
 module.exports = nextConfig 
